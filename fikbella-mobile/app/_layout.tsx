475f3c56540113af1b9d5372008dfa014f82fcd2
Mateus Storti Hellmann <mashuu.sutoruti@gmail.com>
2026-06-24 14:45:34 -0400

    chore: arrumando package de dependencias

--- fikbella-mobile/app/_layout.tsx
+++ fikbella-mobile/app/_layout.tsx
@@ -1,11 +1,8 @@
 import { Stack } from 'expo-router';
 import '../global.css';
-import { View } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <View className="flex-1 min-h-screen">
       <Stack screenOptions={{ headerShown: false }} />
-    </View>
   );
 }
